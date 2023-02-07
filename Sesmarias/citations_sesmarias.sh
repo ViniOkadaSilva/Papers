912c2fe54fe8069aba328cd7ececb3391fde86f4
ViniOkadaSilva <vo10@illinois.edu>
2023-02-07 13:28:16 -0600

    test

---
+++ Sesmarias/citations_sesmarias.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /users/vinicius/OneDrive\ -\ University\ of\ Illinois\ -\ Urbana/Research/Writing/Sesmarias/
+while true; do wget --content-disposition -N https://paperpile.com/eb/buvauEJRqT; sleep 60; done
