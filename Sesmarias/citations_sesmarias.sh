e41d43f336d709e0329b7a2a86e11301daebe4ec
ViniOkadaSilva <vo10@illinois.edu>
2024-01-20 16:38:45 -0600

    test

--- Sesmarias/citations_sesmarias.sh
+++ Sesmarias/citations_sesmarias.sh
@@ -2,3 +2,7 @@
 
 cd /Users/vinicius/Library/texmf/bibtex/bib
 while true; do wget --content-disposition -N https://paperpile.com/eb/buvauEJRqT; sleep 60; done
+
+cd /Users/vinicius/Library/CloudStorage/OneDrive-UniversityofIllinois-Urbana/Research/Writing/git/Sesmarias/02.Draft/02.Second_Draft
+
+while true; do wget --content-disposition -N https://paperpile.com/eb/buvauEJRqT; sleep 60; done
